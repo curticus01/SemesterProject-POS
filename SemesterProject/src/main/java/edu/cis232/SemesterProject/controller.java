bd3f37c94ac65d7c51b79e7e2a1661409e469750
SK207_11 <SK207_11@K207_W811.carrollcc.edu>
2016-05-02 21:17:24 -0400

    added Controller

--- SemesterProject/src/main/java/edu/cis232/SemesterProject/controller.java
+++ SemesterProject/src/main/java/edu/cis232/SemesterProject/controller.java
@@ -1,7 +1,82 @@
 package edu.cis232.SemesterProject;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
 public class controller {
-	public static boolean food = true;
+	public static boolean food = false;
 	public static boolean clothes = false;
 	
+	POS p = new POS();
+	
+    @FXML
+    private ListView<?> listView;
+
+    @FXML
+    private TextField qnt;
+
+    @FXML
+    private Button addToCart;
+
+    @FXML
+    private Button compute;
+
+    @FXML
+    private Button foodBttn;
+
+    @FXML
+    private Button clothesBttn;
+
+    @FXML
+    private TextField subTotal;
+
+    @FXML
+    private TextField price;
+
+    @FXML
+    private TextField name;
+
+    @FXML
+    private TextField total;
+
+    @FXML
+    private Button bttnSubTotal;
+
+    
+    
+    @FXML
+    void SetClothes() {
+    	clothes = true;	
+    }
+
+    @FXML
+    void addItem() throws Exception{
+    	p.addItem(name.getText(), p.getPrice(name.getText()));
+    	price.setText(String.valueOf(p.getPrice(name.getText())));
+    	
+    	
+    }
+
+    @FXML
+    void compute() {
+
+    }
+
+    @FXML
+    void getItem() {
+    	
+    }
+
+    @FXML
+    void setFood() {
+    	food = true;
+    }
+
+    @FXML
+    void subTotal() {
+
+    }
+
 }
